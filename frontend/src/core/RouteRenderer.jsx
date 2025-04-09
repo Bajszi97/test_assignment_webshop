14fcc2d57a54668938f161621416501d91c2c62d
Bajszi97 <bajuszdn@gmail.com>
2025-04-09 10:47:01 +0200

    Remove everything from frontend

--- frontend/src/core/RouteRenderer.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import { RouteContext } from "./RouteServiceProvider"
-import { routes } from "../routes"
-
-export default function RouteRenderer() {
-    const { currentRoute } = useContext(RouteContext)
-    const route = routes.find((r) => r.path === currentRoute)
-    return route? <route.component/> : <h1>404 Page Not Found</h1>
-}
