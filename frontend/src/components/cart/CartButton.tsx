ba9d6d01f930eb2cd22565597627f1571a83a1c4
Bajszi97 <bajuszdn@gmail.com>
2025-04-17 09:10:08 +0200

    Cart button disabled color and data-testid

--- frontend/src/components/cart/CartButton.tsx
+++ frontend/src/components/cart/CartButton.tsx
@@ -10,8 +10,9 @@ const CartButton: React.FC = () => {
       onClick={() => setIsCartOpen(!isCartOpen)}
       className={`relative me-5 ${hasItem ? "group cursor-pointer" : ""}`}
       disabled={!hasItem}
+      data-testid='cart-btn'
     >
-      <Cart className="transition-colors group-hover:fill-primary" />
+      <Cart className={`transition-colors group-hover:fill-primary ${!hasItem? "fill-neutral-400" : ""}`} />
       {hasItem && (
         <span className="absolute -top-3 left-3 flex h-5 min-w-5 items-center justify-center rounded-full bg-shark px-1 font-roboto text-sm font-bold text-white group-hover:bg-primary">
           {cartItems.length}
