130f3cfd9963dcc29a8b50a74d6b8822f801e28e
Bajszi97 <bajuszdn@gmail.com>
2025-04-17 12:20:40 +0200

    Add button wrapper to thumbnails

--- frontend/src/components/gallery/ThumbnailList.tsx
+++ frontend/src/components/gallery/ThumbnailList.tsx
@@ -6,15 +6,16 @@ const ThumbnailList: React.FC<GalleryPartProps> = ({
   setSelectedImage,
 }) => {
   return (
-    <div className="no-scrollbar w-full md:w-24 shrink-0 flex flex-row flex-nowrap gap-x-5 overflow-scroll px-5 md:flex-col md:gap-y-5 md:px-0 md:ps-1 md:pt-1 ">
+    <div className="no-scrollbar w-full md:w-24 shrink-0 flex flex-row flex-nowrap gap-x-5 overflow-scroll px-5 md:flex-col md:gap-y-5 md:p-1">
       {urls.map((url, index) => (
-        <img
-          src={url}
-          alt={`Thumbnail ${index + 1}`}
-          className={`size-20 shrink-0 cursor-pointer  object-contain ${selectedImage === url ? "ring-1 ring-blue-500" : ""
-            }`}
-          onClick={() => setSelectedImage(url)}
-        />
+        <button className="size-fit cursor-pointer" onClick={() => setSelectedImage(url)}>
+          <img
+            src={url}
+            alt={`Thumbnail ${index + 1}`}
+            className={`size-20 shrink-0 object-contain ${selectedImage === url ? "ring-1 ring-blue-500" : ""
+              }`}
+          />
+        </button>
       ))}
     </div>
   );
