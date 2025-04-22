47e1be119ab5ca829389f938743cff20f3bb621b
Bajszi97 <bajuszdn@gmail.com>
2025-04-22 11:55:37 +0200

    Remove redundant routes

--- backend/routes/routes.php
+++ backend/routes/routes.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Controllers\CategoryController;
 use App\Controllers\GraphQLController;
-use App\Controllers\HomeController;
 
 return function (FastRoute\RouteCollector $r) {
-    $r->get("/", [HomeController::class, 'index']);
-    $r->get("/category", [CategoryController::class, 'index']);
-    $r->post("/category", [CategoryController::class, 'store']);
     $r->post("/graphql", [GraphQLController::class, 'handle']);
 };
