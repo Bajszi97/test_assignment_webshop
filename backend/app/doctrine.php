b79c95508d90be6039f6496e40a32d6967794315
Bajszi97 <bajuszdn@gmail.com>
2025-04-22 09:44:50 +0200

    Fix env name change

--- backend/app/doctrine.php
+++ backend/app/doctrine.php
@@ -17,7 +17,7 @@ return function () {
         'driver' => $_ENV['DB_DRIVER'],
         'host' => $_ENV['DB_HOST'],
         'port' => $_ENV['DB_PORT'],
-        'dbname' => $_ENV['DB_DBNAME'],
+        'dbname' => $_ENV['DB_DATABASE'],
         'user' => $_ENV['DB_USER'],
         'password' => $_ENV['DB_PASSWORD'],
     ];
