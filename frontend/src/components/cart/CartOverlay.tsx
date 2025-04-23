240e7cf00502a6f52c015e57cd6dd5f780bdbe06
Bajszi97 <bajuszdn@gmail.com>
2025-04-23 15:15:28 +0200

    Add data testid to cart overlay

--- frontend/src/components/cart/CartOverlay.tsx
+++ frontend/src/components/cart/CartOverlay.tsx
@@ -12,6 +12,7 @@ const CartOverlay: React.FC = () => {
       <div
         className="fixed start-0 top-16 z-[9999] flex h-full w-full justify-end bg-gray-900/20 pb-10"
         onClick={handleClick}
+        data-testid="cart-overlay"
       >
         <CartContent />
       </div>
