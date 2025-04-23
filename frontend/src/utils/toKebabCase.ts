64eda2d130704cfba0c31e5f16984bbe67db82e7
Bajszi97 <bajuszdn@gmail.com>
2025-04-23 11:26:03 +0200

    Make it compatible with tests

--- frontend/src/utils/toKebabCase.ts
+++ frontend/src/utils/toKebabCase.ts
@@ -1,5 +1,11 @@
+// export const toKebabCase = (str: string) => 
+//     str.match(/[A-Z]{2,}(?=[A-Z][a-z0-9]*|\b)|[A-Z]?[a-z0-9]*|[A-Z]|[0-9]+/g)
+//     ?.filter(Boolean)
+//     .map(x => x.toLowerCase())
+//     .join('-') ?? str;
+
 export const toKebabCase = (str: string) => 
-    str.match(/[A-Z]{2,}(?=[A-Z][a-z0-9]*|\b)|[A-Z]?[a-z0-9]*|[A-Z]|[0-9]+/g)
+    str.toLowerCase()
+    .match(/[A-Z]{2,}(?=[A-Z][a-z0-9]*|\b)|[A-Z]?[a-z0-9]*|[A-Z]|[0-9]+/g)
     ?.filter(Boolean)
-    .map(x => x.toLowerCase())
     .join('-') ?? str;
