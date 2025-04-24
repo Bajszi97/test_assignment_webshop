8ff95c9405d570598363fa5982e3e6a7f8e3c70e
Bajszi97 <bajuszdn@gmail.com>
2025-04-24 10:59:08 +0200

    Move quick shop logic into a hook

---
+++ frontend/src/hooks/useQuickShop.ts
@@ -0,0 +1,42 @@
+import { useLazyQuery } from "@apollo/client";
+import { useCartContext } from "./useCartContext";
+import { findProduct } from "@/utils/queries";
+import { Product } from "@/types/DomainModels";
+
+export const useQuickShop = () => {
+    const [getProduct] = useLazyQuery(findProduct);
+    const { addItem, setIsCartOpen } = useCartContext();
+
+    const getDefaultAttributes = (product: Product) => {
+      return product.attributes.reduce<Record<string, string>>(
+        (defaults, attributeSet) => {
+          defaults[attributeSet.slug] = attributeSet.items[0].slug;
+          return defaults;
+        },
+        {},
+      );
+    };
+
+    const quickShopProduct = async (productSlug: string) => {
+        try {
+            const { data } = await getProduct({
+              variables: { slug: productSlug },
+            });
+            
+            if (data?.product) {
+              addItem({
+                product: data.product,
+                quantity: 1,
+                selectedAttributes: getDefaultAttributes(data.product),
+              });
+              setIsCartOpen(true);
+            } else {
+              throw new Error("Could not fetch product for quick shopping.");
+            }
+          } catch (error) {
+            console.error("Error fetching product for quick shopping:", error);
+          }
+    }
+
+    return {quickShopProduct};
+}
--- frontend/src/pages/ProductListPage/components/ProductCard.tsx
+++ frontend/src/pages/ProductListPage/components/ProductCard.tsx
@@ -1,51 +1,22 @@
 import { Link } from "react-router";
-import { Product, ProductForCard } from "@/types/DomainModels";
+import { ProductForCard } from "@/types/DomainModels";
 import { MouseEvent } from "react";
-import { useLazyQuery } from "@apollo/client";
-import { findProduct } from "@/utils/queries";
-import { useCartContext } from "@/hooks/useCartContext";
 import { toKebabCase } from "@/utils/toKebabCase";
 import { PriceTag } from "@/components";
 import { Cart } from "@/assets/icons";
+import { useQuickShop } from "@/hooks/useQuickShop";
 
 interface ProductCardProps {
   product: ProductForCard;
 }
 
-const getDefaultAttributes = (product: Product) => {
-  return product.attributes.reduce<Record<string, string>>(
-    (defaults, attributeSet) => {
-      defaults[attributeSet.slug] = attributeSet.items[0].slug;
-      return defaults;
-    },
-    {},
-  );
-};
-
 export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const outOfStock = !product.inStock;
-  const [getProduct] = useLazyQuery(findProduct);
-  const { addItem, setIsCartOpen } = useCartContext();
-  // TODO move to custom hook
+  const { quickShopProduct } = useQuickShop();
+
   const handleClick = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    try {
-      const { data } = await getProduct({
-        variables: { slug: product.slug },
-      });
-      if (data?.product) {
-        addItem({
-          product: data.product,
-          quantity: 1,
-          selectedAttributes: getDefaultAttributes(data.product),
-        });
-        setIsCartOpen(true);
-      } else {
-        throw new Error("Could not fetch product for quick shopping.");
-      }
-    } catch (error) {
-      console.error("Error fetching product for quick shopping:", error);
-    }
+    quickShopProduct(product.slug);
   };
 
   return (
