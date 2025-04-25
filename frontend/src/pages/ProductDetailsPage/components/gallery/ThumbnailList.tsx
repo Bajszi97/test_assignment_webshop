717c4e1bd938a0f3708ecae57f7906bada2bd3a6
Bajszi97 <bajuszdn@gmail.com>
2025-04-25 09:15:52 +0200

    Fix thumbnail list mobile view

--- frontend/src/pages/ProductDetailsPage/components/gallery/ThumbnailList.tsx
+++ frontend/src/pages/ProductDetailsPage/components/gallery/ThumbnailList.tsx
@@ -6,13 +6,13 @@ export const ThumbnailList: React.FC<GalleryPartProps> = ({
   setSelectedImage,
 }) => {
   return (
-    <div className="no-scrollbar w-full md:w-24 shrink-0 flex flex-row flex-nowrap gap-x-5 overflow-scroll px-5 md:flex-col md:gap-y-5 md:p-1">
+    <div className="no-scrollbar w-full md:w-24 shrink-0 flex flex-row flex-nowrap gap-x-5 py-1 overflow-scroll px-5 md:flex-col md:gap-y-5 md:p-1">
       {urls.map((url, index) => (
-        <button className="size-fit cursor-pointer" onClick={() => setSelectedImage(url)}>
+        <button key={index} className="size-fit shrink-0 cursor-pointer" onClick={() => setSelectedImage(url)}>
           <img
             src={url}
             alt={`Thumbnail ${index + 1}`}
-            className={`size-20 shrink-0 object-contain ${selectedImage === url ? "ring-1 ring-blue-500" : ""
+            className={`size-20 object-contain ${selectedImage === url ? "ring-1 ring-blue-500" : ""
               }`}
           />
         </button>
